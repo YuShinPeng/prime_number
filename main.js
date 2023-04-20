64c6f0e120db863a60f15b47c65e711f3609eed1
YuShinPeng <ccsauce173@gmail.com>
2023-04-20 16:45:50 +0800

    done commit

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
 //DOM節點
-const btn = document.querySelector("#btn");
-const input = document.querySelector("#input")
-const resultOne = document.querySelector("#result");
+const button = document.querySelector("#btn");
+const input = document.querySelector("#input");
+const result = document.querySelector("#result");
+
+let x;
+let y;
+let arr =[];
+let count = 0 ;
+
+//監聽器
+button.addEventListener("click",function(){
+    let num = +input.value;
+    console.log(num);
+    
+    arr = [];
+    for(x = num; x > 0; x--){
+        for(y = 2; y <= x; y++){
+            if(x % y === 0 && x !== y){
+                break;
+            }else if(x % y === 0 && x === y){
+                arr.push(x);
+            }
+        }
+    }
+
+    result.innerHTML = arr;
+
+    
+})
